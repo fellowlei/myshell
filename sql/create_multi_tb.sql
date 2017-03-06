c1c7b6728078e1f9884c67e9041674d38ff4ec77
lulei11 <lulei@jd.com>
2017-03-06 18:59:24 +0800

    test

---
+++ sql/create_multi_tb.sql
@@ -0,0 +1,9 @@
+create database test1;
+use test1;
+create table user_1(id int,name varchar(20),age int);
+create table user_2(id int,name varchar(20),age int);
+
+create database test2;
+use test2;
+create table user_3(id int,name varchar(20),age int);
+create table user_4(id int,name varchar(20),age int);
